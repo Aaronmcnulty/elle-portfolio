f482ea531f276b6aa66b72d08f50bee24a6a6c88
aaronmcnulty <malcomcrabesq@outlook.com>
2025-07-04 22:45:33 +0100

    Build: Install react-router and react-router-dom and configure routes and outlet.

---
+++ src/routes.jsx
@@ -0,0 +1,18 @@
+import App from "./App.jsx";
+import ErrorPage from "./components/errorPage.jsx";
+import LandingPage from "./components/landingPage/LandingPage.jsx";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <LandingPage /> },
+    //   { path: "/home", element: <Homepage /> },
+      
+    ],
+    errorElement: <ErrorPage />,
+  },
+];
+
+export default routes;
